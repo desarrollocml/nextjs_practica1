12bbd8118cc51e91f19fc5751dc631229305ed34
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-02 23:28:07 -0300

    listado de usuarios

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
 import Container from "../components/Container"
 import Head from "next/head"
 import fetch from "isomorphic-unfetch"
+import Users from "../components/Users"
+
 export default function Home(props) {
-  console.log(props)
+ // console.log(props)
   return (
     <Container>
       <Head>
         <title>Next.js - Home</title>
       </Head>
-      <h1>Index </h1>
+      <h1>Next </h1>
+      <Users users={props.users}></Users>
     </Container>
   )
 }
 Home.getInitialProps = async (ctx)=>{
   const res = await fetch("https://reqres.in/api/users");
   const resJSON = await res.json();
-  console.log(resJSON.data);
+  //console.log(resJSON.data);
    return {
     users: resJSON.data,
   }; 
